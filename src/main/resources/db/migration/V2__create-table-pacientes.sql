9142d7df1046c3871c7b1d37d30f041776c02a4e
Aldo Serrano <aldoantonio1723@gmail.com>
2026-01-24 22:53:17 -0600

    feat: agrega tabla pacientes

--- src/main/resources/db/migration/V2__create-table-pacientes.sql
+++ src/main/resources/db/migration/V2__create-table-pacientes.sql
@@ -3,6 +3,7 @@ create table pacientes(
     id bigint not null auto_increment,
     nombre varchar(100) not null,
     email varchar(100) not null unique,
+    telefono varchar(12) not null unique,
     documento varchar(12) not null unique,
     calle varchar(100) not null,
     barrio varchar(100) not null,
