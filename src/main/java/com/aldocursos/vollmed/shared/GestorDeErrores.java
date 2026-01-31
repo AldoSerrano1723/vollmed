67d147419b40e07d79d6ae11ae119317024e0476
Aldo Serrano <aldoantonio1723@gmail.com>
2026-01-31 16:41:02 -0600

    feat: implementar captura de MethodArgumentNotValidException
    - Se añadió un handler para el error 400.
    - Se creó el DTO DatosErrorValidacion para simplificar el JSON.
    - Se filtraron los errores para devolver solo campo y mensaje.

--- src/main/java/com/aldocursos/vollmed/shared/GestorDeErrores.java
+++ src/main/java/com/aldocursos/vollmed/shared/GestorDeErrores.java
@@ -2,8 +2,12 @@ package com.aldocursos.vollmed.shared;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GestorDeErrores {
@@ -11,5 +15,23 @@ public class GestorDeErrores {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity gestionarError404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity TratarError400(MethodArgumentNotValidException e){
+        var errores = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(errores.stream().map(DatosErrorValidacion::new).toList());
+    }
+
+    public record DatosErrorValidacion(
+            String campo,
+            String mensaje
+    ){
+        public DatosErrorValidacion(FieldError fieldError){
+            this(
+                    fieldError.getField(),
+                    fieldError.getDefaultMessage()
+            );
+        }
     }
 }
