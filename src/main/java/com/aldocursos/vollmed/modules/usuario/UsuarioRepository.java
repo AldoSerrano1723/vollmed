34074a66fe4531696e535f88b7823f67fe12ca95
Aldo Serrano <aldoantonio1723@gmail.com>
2026-02-02 16:36:28 -0600

    feat: implementar UserDetailsService y repositorio de usuarios
    - Se creó UsuarioRepository con el método findByLogin.
    - Se creó AutenticacionService implementando UserDetailsService para la lógica de login.

---
+++ src/main/java/com/aldocursos/vollmed/modules/usuario/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.aldocursos.vollmed.modules.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String login);
+}
