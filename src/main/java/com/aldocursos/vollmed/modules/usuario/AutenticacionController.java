4a0bbb1a2327ea6842abdf0e5036f91d3d843d07
Aldo Serrano <aldoantonio1723@gmail.com>
2026-02-05 09:15:30 -0600

    fix: corregir error de AutenticacionController

--- src/main/java/com/aldocursos/vollmed/modules/usuario/AutenticacionController.java
+++ src/main/java/com/aldocursos/vollmed/modules/usuario/AutenticacionController.java
@@ -1,5 +1,6 @@
 package com.aldocursos.vollmed.modules.usuario;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,6 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +21,9 @@ public class AutenticacionController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity iniciarSesion(DatosAutenticacion datosAutenticacion) {
+    public ResponseEntity iniciarSesion(@RequestBody @Valid DatosAutenticacion datosAutenticacion) {
         var token = new UsernamePasswordAuthenticationToken(datosAutenticacion.login(), datosAutenticacion.password());
-        Authentication authentication = manager.authenticate(token);
+        var authentication = manager.authenticate(token);
 
         return ResponseEntity.ok().build();
     }
