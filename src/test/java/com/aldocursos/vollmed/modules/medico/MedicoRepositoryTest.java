e829639e2aed9d99188774921085d01f2ad4b571
Aldo Serrano <aldoantonio1723@gmail.com>
2026-02-19 09:26:24 -0600

    test: agregar estructura inicial para test de MedicoRepository
    - Se inyectó MedicoRepository y EntityManager.
    - Se implementó cálculo dinámico de fecha usando TemporalAdjusters.
    - Se añadió aserción con AssertJ para validar que retorna null cuando no hay disponibilidad.
    - Se preparó la estructura para métodos utilitarios de creación de datos.

--- src/test/java/com/aldocursos/vollmed/modules/medico/MedicoRepositoryTest.java
+++ src/test/java/com/aldocursos/vollmed/modules/medico/MedicoRepositoryTest.java
@@ -1,18 +1,41 @@
 package com.aldocursos.vollmed.modules.medico;
 
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class MedicoRepositoryTest {
 
+    @Autowired
+    private MedicoRepository repository;
+
+    @Autowired
+    private EntityManager em;
+
     @Test
+    @DisplayName("Deveria de devolver null cuando el medico buscado no esta disponible en esa fecha")
     void eleqirMedicoAleatorioPorEspecialidad() {
+
+        var lunesSiguienteALas10 = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10, 0);
+
+//        var medico =
+//        var paciente =
+//        var consulta =
+
+        var medicoLibre = repository.eleqirMedicoAleatorioPorEspecialidad(Especialidad.CARDIOLOGIA, lunesSiguienteALas10);
+        assertThat(medicoLibre).isNull();
     }
 }
